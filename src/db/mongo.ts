a76788ba98cb0a3c4a597da5ea32706984095945
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2023-09-29 12:38:50 +0300

    com_1

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -2,7 +2,7 @@ import {client} from "../repositories/DB";
 import {PostsDbModel} from "../model/posts-model";
 import {BlogsDbModels} from "../model/blogs-db-models";
 
-export const dbBlogs = client.db("hw2").collection<BlogsDbModels>("blogs")
+export const dbBlogs = client.db("hw3").collection<BlogsDbModels>("blogs")
 
 
-export const dbPosts = client.db("hw2").collection<PostsDbModel>("posts")
+export const dbPosts = client.db("hw3").collection<PostsDbModel>("posts")
